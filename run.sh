430a0970ebadb145a32453e6c739fc857bcaaf10
Mayank-MP05 <mayank5pande@gmail.com>
2022-08-17 11:43:42 +0530

    [SETUP] Added front end serving mechanism script

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Clear any existing running processes
+pm2 delete all
 
+# Starting Frontend in No-Demon Mode
+echo "[FRONTEND] Starting Server ..."
+pm2 serve client-draw/build 3000 --spa 
+
+# Starting backend in No-Demon Mode
 echo "[BACKEND] Starting Server ..."
-pm2 start server-draw/index.js
+pm2 start server-draw/index.js --no-daemon
+
 
